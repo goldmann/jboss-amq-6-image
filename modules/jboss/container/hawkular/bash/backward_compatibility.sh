8e2e4cb26b6ab398fac917f977ae89a97a107967
Marek Goldmann <mgoldman@redhat.com>
2018-11-28 15:23:26 +0100

    Use correct module

--- modules/jboss/container/hawkular/bash/backward_compatibility.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set up Hawkular for java s2i builder image
-set -e
-
-ln -s /opt/jboss/container/hawkular /opt/hawkular
-
-chown -h jboss:root /opt/hawkular
--- modules/jboss/container/openjdk/base/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use /dev/urandom to speed up startups.
-echo securerandom.source=file:/dev/urandom >> /usr/lib/jvm/java/jre/lib/security/java.security
